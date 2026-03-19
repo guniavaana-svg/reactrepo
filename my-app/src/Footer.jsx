59f3a29fb3682dca74a9e6dbb5f0ce23433a34cb
unknown <GuniavaAna@itvet.ge>
2026-03-19 10:13:04 -0700

    second

---
+++ my-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} your website</p>
+        </footer>
+    )
+}
+export default Footer;
---
+++ my-app/src/MainSlider.jsx
@@ -0,0 +1,8 @@
+function MainSlider(){
+    const food1="orange";
+    const food2="banana";
+    return(
+        <span>this is MainSlider</span>
+    )
+}
+export default MainSlider
